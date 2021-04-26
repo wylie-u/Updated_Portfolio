42b3617d7fe06e1b5ec1975736499efa6ba0e216
wylie-u <wylieurig@gmail.com>
2021-04-25 20:59:03 -0600

    updated port

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// const menu = document.querySelector('.menu');
+// const x = document.querySelector('.logo__symbol');
+// const close = document.querySelector('.closeNav');
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+// x.addEventListener('click', toggleOpen);
+// close.addEventListener('click', toggleClose);
